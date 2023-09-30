1faba35b99af094167066b5fa23a2d5d9ae3094c
K33VY <lkeevy01@gmail.com>
2023-09-30 14:22:39 +1000

    Updated CSS to Grid
    - Changed from using FlexBox to grid as it should be easier to use.
    - Working way better already, just need to have contentRight stay on the right

--- onlycars/src/App.js
+++ onlycars/src/App.js
@@ -1,23 +1,38 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home.js";
+import "./App.css";
 
 function App() {
-  return (
-    <Router>
-      <nav className="navBar">
-        <h1>OnlyCars</h1>
-        <section className="linkSection">
-          <Link className="links" to="/">
-            Home
-          </Link>
-        </section>
-      </nav>
+    return (
+        <div class="mainContainer">
+            <div class="NavBar"></div>
+            <div class="spaceLeft"></div>
+            <Router>
+                <aside className="contentLeft">
+                    <h1>OnlyCars</h1>
+                    <nav className="innerNav">
+                        <Link className="links" to="/">
+                            Home
+                        </Link>
+                        <Link className="links" to="/">
+                            Cars
+                        </Link>
+                        <Link className="links" to="/">
+                            Creators
+                        </Link>
+                    </nav>
+                </aside>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+                <div class="pageContent">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </div>
+            </Router>
+            <div class="spaceRight"></div>
+            <div class="Footer"></div>
+        </div>
+    );
 }
 
 export default App;
--- onlycars/src/Home.js
+++ onlycars/src/Home.js
@@ -1,10 +1,22 @@
 const Home = () => {
     return (
-      <div className="mainContainer">
-        <h1>COMP3120: Advanced Web Development</h1>
-        <p>Welcome to our Unit app. You have not logged in.</p>
-      </div>
-    );
-  };
+        <div >
+            <div className="contentMid">
+                <h1>COMP3120: Advanced Web Development</h1>
+                <p>Welcome to our Unit app. You have not logged in.</p>
+            </div>
 
-  export default Home;
+            <div className="contentRight">
+                <ul>
+                  <li>content</li>
+                  <li>content</li>
+                  <li>content</li>
+                  <li>content</li>
+                  <li>content</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
