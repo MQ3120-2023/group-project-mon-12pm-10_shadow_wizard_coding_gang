a27901b6144a562fd259c6389fa6495c6388cc6e
K33VY <lkeevy01@gmail.com>
2023-10-16 00:30:45 +1100

    Update HomePage.js
    - Started adding basic elements to plan out how everything is going to go in the HTML and what components will need to be created

--- onlycars/src/pages/HomePage.js
+++ onlycars/src/pages/HomePage.js
@@ -2,29 +2,29 @@ import SearchBar from "../components/SearchBar";
 
 const HomePage = () => {
     return (
-        
         <div id="main-container">
-            
-            <div id="header">
-                
-            </div>
+            <div id="header"></div>
 
-            <div id="logo-container">
-            </div>
+            <div id="logo-container"></div>
 
             <div id="search-container">
                 <SearchBar></SearchBar>
             </div>
 
-            <div id="profile-container">
-            </div>
+            <div id="profile-container"></div>
 
             <div id="posts-container">
+
+                <div id="sorting-container">
+                    <button type="button">Latest Posts</button>
+                    <button type="button">Popular posts</button>
+                </div>
+
+                
+
             </div>
 
-            <div id="-container">
-            </div>
-
+            <div id="info-container"></div>
         </div>
     );
 };
