701ff6c39cc1e4e529d625715e0f2cf171c66dcf
K33VY <lkeevy01@gmail.com>
2023-10-26 18:02:20 +1100

    Website connected to MongoDB
    - Managed to figure out the error and got the server connecting to MongoDB
    - Got rid of '.env' and just created a const in 'server.js' and used that.

--- onlycars/server/server.js
+++ onlycars/server/server.js
@@ -1,15 +1,13 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const mongoose = require('mongoose');
-
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const mongoose = require("mongoose");
 const app = express();
 
-// Middleware
-  // Import Mongoose Schemas and Models
-  const userSchema = new mongoose.Schema({
+
+// Import Mongoose Schemas and Models
+const userSchema = new mongoose.Schema({
     userId: String,
     username: String,
     email: String,
@@ -19,61 +17,64 @@ const app = express();
     description: String,
     cars: Number,
     posts: Number,
-    subscribers: Number
-  });
-  const User = mongoose.model('User', userSchema);
+    subscribers: Number,
+});
+const User = mongoose.model("User", userSchema);
 
-  // Car Schema and Model
-  const carSchema = new mongoose.Schema({
+// Car Schema and Model
+const carSchema = new mongoose.Schema({
     carId: String,
     owner: String,
     ownership: String,
     brandModel: String,
     year: String,
     modifications: String,
-    img: String
-  });
-  const Car = mongoose.model('Car', carSchema);
+    img: String,
+});
+const Car = mongoose.model("Car", carSchema);
 
-  // Post Schema and Model
-  const postSchema = new mongoose.Schema({
+// Post Schema and Model
+const postSchema = new mongoose.Schema({
     postId: String,
     author: String,
     content: String,
     img: String,
     likes: Number,
     comments: Number,
-    shares: Number
-  });
-  const Post = mongoose.model('Post', postSchema);
-  
+    shares: Number,
+});
+const Post = mongoose.model("Post", postSchema);
+
 // MongoDB URI
-const MONGODB_URI = process.env.MONGODB_URI;
+const uri =
+    process.env.MONGODB_URI ||
+    "mongodb+srv://ShadowWizard:rFBiWdZ4jFRW6RMI@onlycars.rcvamax.mongodb.net/onlycars?retryWrites=true&w=majority";
 
 // Mongoose connection
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('Error connecting to MongoDB:', error.message);
-  });
+mongoose
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.log("Error connecting to MongoDB:", error.message);
+    });
 
 app.use(cors());
 app.use(bodyParser.json());
-const path = require('path');
-app.use('/images', express.static(path.join(__dirname, 'images')));
+const path = require("path");
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 // Log static file requests
-app.use('/images', (req, res, next) => {
-  console.log(`Static file request for: ${req.url}`);
-  next();
+app.use("/images", (req, res, next) => {
+    console.log(`Static file request for: ${req.url}`);
+    next();
 });
 
 // TODO: Replace this with MongoDB data retrieval
 
 // Importing data from data.json
-const data = require('./data.json');
+const data = require("./data.json");
 
 // Read users from data.json
 const users = data.users;
@@ -82,65 +83,74 @@ const users = data.users;
 const posts = data.posts;
 
 // Endpoint to get all posts
-app.get('/getPosts', (req, res) => {
-  res.status(200).json(posts);
+app.get("/getPosts", (req, res) => {
+    res.status(200).json(posts);
 });
 
 // Endpoint to get the current user's info
-app.get('/getCurrentUser', (req, res) => {
-  const { username } = req.query;
-  const user = users.find(u => u.username === username);
-  if (user) {
-    res.status(200).json([user]);  // Wrap the user object in an array
-  } else {
-    res.status(404).json({ message: 'User not found' });
-  }
+app.get("/getCurrentUser", (req, res) => {
+    const { username } = req.query;
+    const user = users.find((u) => u.username === username);
+    if (user) {
+        res.status(200).json([user]); // Wrap the user object in an array
+    } else {
+        res.status(404).json({ message: "User not found" });
+    }
 });
 
 // TODO: Replace this with MongoDB-based authentication
 
 // Basic Authentication
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  console.log('Received request:', req.body);
-  const user = users.find(u => u.username === username && u.password === password);
-  if (user) {
-    console.log('Sending response:', { message: 'Login successful' });
-    res.status(200).json({ message: 'Login successful' });
-  } else {
-    console.log('Sending response:', { message: 'Invalid users' });
-    res.status(401).json({ message: 'Invalid users' });
-  }
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    console.log("Received request:", req.body);
+    const user = users.find(
+        (u) => u.username === username && u.password === password
+    );
+    if (user) {
+        console.log("Sending response:", { message: "Login successful" });
+        res.status(200).json({ message: "Login successful" });
+    } else {
+        console.log("Sending response:", { message: "Invalid users" });
+        res.status(401).json({ message: "Invalid users" });
+    }
 });
 
 // Credential Creation
-app.post('/signup', (req, res) => {
-  const { username, email, password } = req.body;
-  console.log('Received request:', req.body);
-  const user = users.find(u => u.username === username);
-  if (user) {
-    console.log('Sending response:', { message: 'Username already exists' });
-    res.status(409).json({ message: 'Username already exists' });
-  } else {
-    users.push({ username, email, password });
-    fs.writeFileSync('./server/data.json', JSON.stringify({ users: users, posts: posts }));
-    console.log('Sending response:', { message: 'SignUp successful' });
-    res.status(200).json({ message: 'SignUp successful' });
-  }
+app.post("/signup", (req, res) => {
+    const { username, email, password } = req.body;
+    console.log("Received request:", req.body);
+    const user = users.find((u) => u.username === username);
+    if (user) {
+        console.log("Sending response:", {
+            message: "Username already exists",
+        });
+        res.status(409).json({ message: "Username already exists" });
+    } else {
+        users.push({ username, email, password });
+        fs.writeFileSync(
+            "./server/data.json",
+            JSON.stringify({ users: users, posts: posts })
+        );
+        console.log("Sending response:", { message: "SignUp successful" });
+        res.status(200).json({ message: "SignUp successful" });
+    }
 });
 
 // Session management
-const session = require('express-session');
+const session = require("express-session");
 
-app.use(session({
-  secret: 'onlycars_secret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+app.use(
+    session({
+        secret: "onlycars_secret",
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false },
+    })
+);
 
 // Start the server
 const port = 3001;
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
