e9996952a481e727223faf4ca66d1199b1979478
Victor-Quocson <victor.quocson@students.mq.edu.au>
2023-11-02 18:56:40 +1100

    Styled Settings Front-end
    Completed settings styling, will need to update if more input required.
    #56

--- onlycars/src/components/SettingsSecurity.js
+++ onlycars/src/components/SettingsSecurity.js
@@ -2,56 +2,58 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const SettingsSecurity = () => {
-    const [password, setPassword] = useState("");
-    const [conPassword, setConPassword] = useState("");
+	const [password, setPassword] = useState("");
+	const [conPassword, setConPassword] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post(
-                "/SettingsSecurity",
-                {
-                    password,
-                }
-            );
-            const { data } = response;
-            if (data.message === "Settings Saved") {
-                // Redirect to home page or dashboard
-                alert("Your password has been changed!");
-            }
-        } catch (error) {
-            console.error("Error during authentication:", error);
-            alert("An error occurred during login. Please try again.");
-        }
-    };
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await axios.post("/SettingsSecurity", {
+				password,
+			});
+			const { data } = response;
+			if (data.message === "Settings Saved") {
+				// Redirect to home page or dashboard
+				alert("Your password has been changed!");
+			}
+		} catch (error) {
+			console.error("Error during authentication:", error);
+			alert("An error occurred during login. Please try again.");
+		}
+	};
 
-    return (
-        <main id="main-container">
-            <div id="info-box">
-                <p id="info-header">Security Settings</p>
-                <div id="info-list">
-                    <form onSubmit={handleSubmit}>
-                        <h3>Update Password:</h3>
-                        <input
-                            type="text"
-                            value={password}
-                            placeholder="New Password"
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                        <input
-                            type="text"
-                            value={conPassword}
-                            placeholder="Confirm Password"
-                            onChange={(e) => setConPassword(e.target.value)}
-                            required
-                        />
-                        <button type="submit">Save Changes</button>
-                    </form>
-                </div>
-            </div>
-        </main>
-    );
+	return (
+		<main id="main-container">
+			<div className="settings-box">
+				<div className="settings-inside-box">
+					<p className="settings-header">Profile Settings</p>
+					<div className="settings-list">
+						<form onSubmit={handleSubmit}>
+							<label>Update Password:</label>
+							<input
+								type="text"
+								value={password}
+								placeholder="New Password"
+								onChange={(e) => setPassword(e.target.value)}
+								required
+							/>
+							<hr />
+                            <label></label>
+							<input
+								type="text"
+								value={conPassword}
+								placeholder="Confirm Password"
+								onChange={(e) => setConPassword(e.target.value)}
+								required
+							/>
+							<hr />
+							<button type="submit">Save Changes</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default SettingsSecurity;
--- onlycars/src/components/SettingsUser.js
+++ onlycars/src/components/SettingsUser.js
@@ -2,83 +2,83 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const SettingsUser = () => {
-    const [email, setEmail] = useState("");
-    const [location, setLocation] = useState("");
-    const [gender, setGender] = useState("");
-    const [language, setLanguage] = useState("");
-    const [theme, setTheme] = useState("");
+	const [email, setEmail] = useState("");
+	const [location, setLocation] = useState("");
+	const [gender, setGender] = useState("");
+	const [language, setLanguage] = useState("");
+	const [theme, setTheme] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post(
-                "/SettingsUser",
-                {
-                    email,
-                    location,
-                    gender,
-                    language,
-                    theme,
-                }
-            );
-            const { data } = response;
-            if (data.message === "Settings Saved") {
-                // Redirect to home page or dashboard
-                alert("Your Changes have been applied!");
-            }
-        } catch (error) {
-            console.error("Error during authentication:", error);
-            alert("An error occurred during login. Please try again.");
-        }
-    };
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await axios.post("/SettingsUser", {
+				email,
+				location,
+				gender,
+				language,
+				theme,
+			});
+			const { data } = response;
+			if (data.message === "Settings Saved") {
+				// Redirect to home page or dashboard
+				alert("Your Changes have been applied!");
+			}
+		} catch (error) {
+			console.error("Error during authentication:", error);
+			alert("An error occurred during login. Please try again.");
+		}
+	};
 
-    return (
-        <main id="main-container">
-            <div id="info-box">
-                <p id="info-header">User Settings</p>
-                <div id="info-list">
-                    <form onSubmit={handleSubmit}>
-                        <label>Email</label>
-                        <input
-                            type="text"
-                            value={email}
-                            placeholder="Email"
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-
-                        <label>Location</label>
-                        <input
-                            type="text"
-                            value={location}
-                            placeholder="Country, City/Town"
-                            onChange={(e) => setLocation(e.target.value)}
-                            required
-                        />
-
-                        <label>Gender</label>
-                        <input
-                            type="text"
-                            value={gender}
-                            placeholder="Gender"
-                            onChange={(e) => setGender(e.target.value)}
-                            required
-                        />
-
-                        <label>Language</label>
-                        <input
-                            type="text"
-                            value={language}
-                            placeholder="Language"
-                            onChange={(e) => setLanguage(e.target.value)}
-                            required
-                        />
-                        <button type="submit">Save Changes</button>
-                    </form>
-                </div>
-            </div>
-        </main>
-    );
+	return (
+		<main id="main-container">
+			<div className="settings-box">
+				<div className="settings-inside-box">
+					<p className="settings-header">User Settings</p>
+					<div className="settings-list">
+						<form onSubmit={handleSubmit}>
+							<label>Email</label>
+							<input
+								type="text"
+								value={email}
+								placeholder="Email"
+								onChange={(e) => setEmail(e.target.value)}
+								required
+							/>
+							<hr />
+							<label>Location</label>
+							<input
+								type="text"
+								value={location}
+								placeholder="Suburb/City, Country"
+								onChange={(e) => setLocation(e.target.value)}
+								required
+							/>
+							<hr />
+							<label>Gender</label>
+							<input
+								type="text"
+								value={gender}
+								placeholder="Gender"
+								onChange={(e) => setGender(e.target.value)}
+								required
+							/>
+							<hr />
+							<label>Language</label>
+							<input
+								type="text"
+								value={language}
+								placeholder="Language"
+								onChange={(e) => setLanguage(e.target.value)}
+								required
+							/>
+							<hr />
+							<button type="submit">Save Changes</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default SettingsUser;
