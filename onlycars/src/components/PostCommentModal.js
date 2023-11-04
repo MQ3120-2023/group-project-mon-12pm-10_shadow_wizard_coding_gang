5cbaf8919045fd5ef22e92bb004f5ea1763497b5
Victor-Quocson <victor.quocson@students.mq.edu.au>
2023-11-04 23:07:56 +1100

    Added Front-End for Comments
    Added the front end container for comments, found that comments could not be displayed due to:
    - Comments not on MongoDB yet
    - Comments contain incorrect data in Data.json
    - Comments map uses "user" object which does not exist
    
    #69

--- onlycars/src/components/PostCommentModal.js
+++ onlycars/src/components/PostCommentModal.js
@@ -12,18 +12,15 @@ const PostCommentModal = ({
 	car,
 	user,
 }) => {
-	const [comments, setComments] = useState([]);
+	const [comments, setComments] = useState([1]);
 	const navigate = useNavigate();
 
 	useEffect(() => {
-		console.log("Post object for Comments: " + post);
-		console.log("PostId for Comments Post: " + postId);
-		console.log("Car for Comments Post: " + car);
-		console.log("User for Comments Post: " + user);
 		if (isOpen) {
 			fetch(`http://localhost:3001/getComments/${postId}`)
 				.then((response) => response.json())
 				.then((data) => {
+					console.log("Comments Data: " + data);
 					setComments(data);
 				})
 				.catch((error) => {
@@ -54,7 +51,7 @@ const PostCommentModal = ({
 			className="comment-modal"
 			overlayClassName="overlay"
 		>
-			<section className="post-container">
+			{/* <section className="post-container">
 				<header className="post-header" onClick={() => handleUserClick(user)}>
 					<img
 						className="post-pfp"
@@ -97,35 +94,40 @@ const PostCommentModal = ({
 						Like
 					</button>
 				</div>
-			</section>
+			</section> */}
 
-			{comments.map((comm, index) => {
-				console.log("Generating Post & Comments");
-				const user = comm.user;
-				return (
-					<section className="comment-container" key={index}>
-						<header
-							className="comment-header"
-							onClick={() => handleUserClick(user)}
-						>
-							<img
-								className="comment-pfp"
-								src={user.profilepic}
-								alt="User Profile Picture"
-								style={{ cursor: "pointer" }}
-							/>
-							<a className="comment-user" style={{ cursor: "pointer" }}>
-								{user.username}
-							</a>
-						</header>
-						<p>
-							{post.description}
-							<br />
-							{/* {moment(post.date).format("MMMM Do YYYY, h:mm a")} */}
-						</p>
-					</section>
-				);
-			})}
+			<div className="comments-div">
+				<section className="comments-section">
+					<p id="info-header">Comments</p>
+					{comments.map((comm, index) => {
+						console.log("Generating Post & Comments");
+						const user = comm.userId;
+						return (
+							<section className="comment-container" key={index}>
+								<header
+									className="comment-header"
+									onClick={() => handleUserClick(user)}
+								>
+									<img
+										className="comment-pfp"
+										src={user.profilepic}
+										alt="User Profile Picture"
+										style={{ cursor: "pointer" }}
+									/>
+									<a className="comment-user" style={{ cursor: "pointer" }}>
+										{user.username}
+									</a>
+								</header>
+								<p>
+									{post.description}
+									<br />
+									{/* {moment(post.date).format("MMMM Do YYYY, h:mm a")} */}
+								</p>
+							</section>
+						);
+					})}
+				</section>
+			</div>
 		</Modal>
 	);
 };
