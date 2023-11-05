e4f404dfb5807e561dc6b5973cd7da1e75665882
K33VY <lkeevy01@gmail.com>
2023-11-05 13:50:17 +1100

    Resolved Profile Page Issues
    Resolved #88

--- onlycars/src/components/Profile.js
+++ onlycars/src/components/Profile.js
@@ -1,23 +1,21 @@
-import React, {useContext} from "react";
-import { CurrentUserContext } from "../App";
+import React from "react";
 const Profile = ({ user }) => {
-    const currentUser = useContext(CurrentUserContext);
     return (
         <section>
             <section className="profile-container">
                 <img
                     className="profile-banner"
-                    src={currentUser?.currentUser?.profilepic}
+                    src={user?.profilepic}
                     alt="Profile Banner"
                 />
                 <header className="profile-header">
-                    <h1 className="profile-user">{currentUser?.currentUser?.username}</h1>
-                    <sub className="profile-location">{currentUser?.currentUser?.location}</sub>
+                    <h1 className="profile-user">{user?.username}</h1>
+                    <sub className="profile-location">{user?.location}</sub>
                 </header>
-                <p>{currentUser?.currentUser?.description}</p>
+                <p>{user?.description}</p>
                 <aside id="profile-info">
-                    <p>Cars Owned: {currentUser?.currentUser?.cars}</p>
-                    <p>Posts Made: {currentUser?.currentUser?.posts}</p>
+                    <p>Cars Owned: {user?.cars}</p>
+                    <p>Posts Made: {user?.posts}</p>
                 </aside>
             </section>
             {/* )
