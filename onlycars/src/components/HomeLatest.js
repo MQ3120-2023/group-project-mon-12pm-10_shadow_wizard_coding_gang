053c48aea91495b14e3f412ed1d18d500f836341
Victor-Quocson <victor.quocson@students.mq.edu.au>
2023-11-05 14:56:26 +1100

    Completed Front-End for Comments Modal
    Completed the Front-End for Comments Modal, added the form for new comments and resolved the errors regarding displaying the posts and comments.
    
    #69

--- onlycars/src/components/HomeLatest.js
+++ onlycars/src/components/HomeLatest.js
@@ -142,7 +142,7 @@ const HomeLatest = ({ sortType }) => {
 								<button
 									id="comment-button"
 									className="post-button"
-									onClick={() => handleCommentClick(post, post.postId, car, user)}
+									onClick={() => handleCommentClick(post, post.postId, car, user, closeImageModal)}
 								>
 									Comment
 								</button>
