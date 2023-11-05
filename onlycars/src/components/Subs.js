d584978a1eb2a60b78fb1d331dd442c9150c2fc6
Victor-Quocson <victor.quocson@students.mq.edu.au>
2023-11-05 18:34:11 +1100

    Added No Subs message

--- onlycars/src/components/Subs.js
+++ onlycars/src/components/Subs.js
@@ -5,48 +5,53 @@ import { useNavigate } from "react-router-dom";
 
 const Subs = ({ path }) => {
 	const currentUser = useContext(CurrentUserContext);
-    const [subs, setSubs] = useState([]);
+	const [subs, setSubs] = useState([]);
 	const navigate = useNavigate();
 
-    useEffect(() => {
-        axios
-            .get(`/getUserSubs?userId=${currentUser.currentUser.userId}`)
-            .then((response) => {
-                setSubs(response.data);
-            })
-            .catch((error) => {
-                console.error("Error fetching data:", error);
-            });
-    }, [currentUser]); // Add currentUser as a dependency for the effect
+	useEffect(() => {
+		axios
+			.get(`/getUserSubs?userId=${currentUser.currentUser.userId}`)
+			.then((response) => {
+				setSubs(response.data);
+			})
+			.catch((error) => {
+				console.error("Error fetching data:", error);
+			});
+	}, [currentUser]); // Add currentUser as a dependency for the effect
 
-    const handleUserClick = (user) => {
+	const handleUserClick = (user) => {
 		// Navigate to the ProfilePage with the username as a parameter
 		// and pass the user data as state
 		navigate(`/profile`, { state: { user } });
 	};
 
-    return (
-        <section id="posts-container">
-            {subs.map((sub, index) => (
-                <section className="sub-container" key={index}>
-                    <div className="sub-left" onClick={() => handleUserClick(sub)}>
-                        <img
-                            className="sub-pfp"
-                            src={sub.profilepic}
-                            alt="User Profile Picture"
-                            style={{ cursor: "pointer" }}
-                        />
-                        <a className="sub-user" style={{ cursor: "pointer" }}>{sub.username}</a>
-                    </div>
-                    <div className="sub-right">
-                        <p>Posts Made: {sub.posts}</p>
-                        <p>Subscribers: {sub.subscribers}</p>
-                        <p>Cars Owned: {sub.cars}</p>
-                    </div>
-                </section>
-            ))}
-        </section>
-    );
+	return (
+		<section id="posts-container">
+			{subs.map((sub, index) => (
+				<section className="sub-container" key={index}>
+					<div className="sub-left" onClick={() => handleUserClick(sub)}>
+						<img
+							className="sub-pfp"
+							src={sub.profilepic}
+							alt="User Profile Picture"
+							style={{ cursor: "pointer" }}
+						/>
+						<a className="sub-user" style={{ cursor: "pointer" }}>
+							{sub.username}
+						</a>
+					</div>
+					<div className="sub-right">
+						<p>Posts Made: {sub.posts}</p>
+						<p>Subscribers: {sub.subscribers}</p>
+						<p>Cars Owned: {sub.cars}</p>
+					</div>
+				</section>
+			))}
+			<p style={{ textAlign: "center" }}>
+				<b>No Posts? Go to Explore for more posts!</b>
+			</p>
+		</section>
+	);
 };
 
 export default Subs;
